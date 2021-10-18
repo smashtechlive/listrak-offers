005f12e1fe26de99f8e7cd9b4eaa4a21feffd22b
Johnny P <jphilavanh@gmail.com>
2021-10-18 12:48:49 -0700

    initial class test

---
+++ index.js
@@ -0,0 +1,17 @@
+class Listrak {
+    constructor() {
+
+    }
+
+    init(listrakId) {
+        this.listrakId = listrakId;
+    }
+
+    test() {
+        console.log('jp - hello test');
+    }
+
+
+}
+
+module.exports = new Listrak();
